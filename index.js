89373e38ac2f0ec669466547493686b11ab427e8
Gopal Balasubramanian <gopalb@brandeis.edu>
2021-11-12 22:09:43 -0500

    changed index.html to index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index.html'))
+  .get('/', (req, res) => res.render('index'))
   .get('/cool', (req, res) => res.send(cool()))
 .get('/db', async (req, res) => {
     try {
